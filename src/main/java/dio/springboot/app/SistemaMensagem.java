f17200b286130eefca61feba9f7d6801dc5eee4e
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-08-14 10:23:21 -0300

    (aula ConfigurationProperties)

--- src/main/java/dio/springboot/app/SistemaMensagem.java
+++ src/main/java/dio/springboot/app/SistemaMensagem.java
@@ -13,12 +13,12 @@ import java.util.List;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner{
-
+    private Remetente remetente;
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviada por: " + nome
-                + "\nE-mail: " + email
-                + "\nCom telefones para contato: " + telefones);
+        System.out.println("Mensagem enviada por: " + remetente.getNome()
+                + "\nE-mail: " + remetente.getEmail()
+                + "\nCom telefones para contato: " + remetente.getTelefones());
         System.out.println("Seu cadastro foi aprovado...");
     }
 }
